4c6621ca6f9e77e5e3f54147fd49d780328c5b96
michele <daros@arrival.com>
2021-11-14 03:01:46 +0100

    reorganize code

--- simulate-switching-workspaces-on-active-monitor@micheledaros.com/extension.js
+++ simulate-switching-workspaces-on-active-monitor@micheledaros.com/extension.js
@@ -1,361 +1,303 @@
-const Gio = imports.gi.Gio;
-const Clutter = imports.gi.Clutter;
-const GLib = imports.gi.GLib;
-const St = imports.gi.St;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
-const Mainloop = imports.mainloop;
-const GObject = imports.gi.GObject;
-
-const Gettext = imports.gettext.domain("gnome-shell-extensions");
-const _ = Gettext.gettext;
-
 const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Slider = imports.ui.slider;
-
 const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
 
 const SCHEMA =
-  "org.gnome.shell.extensions.simulate-switching-workspaces-on-active-monitor";
+    "org.gnome.shell.extensions.simulate-switching-workspaces-on-active-monitor";
 const HOTKEY_NEXT = "switch-to-next-workspace-on-active-monitor";
 const HOTKEY_PREVIOUS = "switch-to-previous-workspace-on-active-monitor";
 
 const UP = -1;
 const DOWN = 1;
-
 const DEBUG_ACTIVE = true;
 
 class WindowWrapper {
-  constructor(windowActor, monitorIndex, workspaceIndex) {
-    this.windowActor = windowActor;
-    this.metaWindow = this.windowActor.get_meta_window();
-    this.windowType = this.metaWindow.get_window_type();
-    this.initialMonitorIndex = this.getMonitorIndex();
-    this.initialWorkspaceIndex = this.getWorkspaceIndex();
-  }
+    constructor(windowActor) {
+        this.windowActor = windowActor;
+        this.metaWindow = this.windowActor.get_meta_window();
+        this.windowType = this.metaWindow.get_window_type();
+        this.initialMonitorIndex = this.getMonitorIndex();
+        this.title = this.getTitle();
+    }
 
-  getTitle() {
-    return this.metaWindow.get_title();
-  }
+    getTitle() {
+        return this.metaWindow.get_title();
+    }
 
-  getWorkspaceIndex() {
-    return this.metaWindow.get_workspace().index();
-  }
+    getWorkspaceIndex() {
+        return this.metaWindow.get_workspace().index();
+    }
 
-  getMonitorIndex() {
-    return this.metaWindow.get_monitor();
-  }
+    getMonitorIndex() {
+        return this.metaWindow.get_monitor();
+    }
 
-  toString() {
-    return `
+    isNormal() {
+        return this.windowType === Meta.WindowType.NORMAL;
+    }
+
+    moveToWorkSpace(nextIndex) {
+        this.metaWindow.change_workspace_by_index(nextIndex, false);
+    }
+
+    toString() {
+        return `
             title: ${this.getTitle()}
             windowType: ${this.windowType}
             monitorIndex: ${this.getMonitorIndex()}
             workspaceIndex: ${this.getWorkspaceIndex()}
             isNormal: ${this.isNormal()}
             `;
-  }
-
-  isNormal() {
-    return this.windowType === Meta.WindowType.NORMAL;
-  }
-
-  moveToWorkSpace(nextIndex) {
-    this.metaWindow.change_workspace_by_index(nextIndex, false);
-  }
+    }
 }
 
 class WorkSpacesService {
-  constructor(_configurationService) {
-    this._configurationService = _configurationService;
-    this._initNWorskpaces();
-    this._activeWorkspaceIndex =
-      global.workspace_manager.get_active_workspace_index();
-    this._monitorActivatedWithFocus=null;
-  }
-
-  _initNWorskpaces() {
-    this._nWorkspaces = global.workspace_manager.get_n_workspaces();
-    maybeLog(`  workspacesService.nWorkspaces: ${this._nWorkspaces} `);
-  }
-
-  _moveToDirection(windowWrapper, direction) {
-    let nextWorkspace =
-      (this._nWorkspaces + windowWrapper.getWorkspaceIndex() + direction) %
-      this._nWorkspaces;
-    maybeLog(`next workspace will be ${nextWorkspace}`);
-    windowWrapper.moveToWorkSpace(nextWorkspace);
-  }
-
-  windowActivatedWithFocus(window) {
-    if (window.get_workspace().index() != global.workspace_manager.get_active_workspace_index()) {
-      let monitor = window.get_monitor();
-      log(`next active monitor will be ${monitor}`)
-      this._monitorActivatedWithFocus = monitor;
-    }
-  }
-
-  switchToPreviouslyActiveWorkspaceOnInactiveMonitors() {
-    if (!this._configurationService.automaticSwitchingIsEnabled()) {
-      maybeLog(
-        `not switching to previously active workspace because automaticSwitching is disabled`
-      );
-      return 
+    constructor(_configurationService) {
+        this._configurationService = _configurationService;
+        this._initNWorskpaces();
+        this._activeWorkspaceIndex =
+            this._getActiveWorkspaceIndex();
+        this._monitorActivatedWithFocus = null;
     }
 
-    this._initNWorskpaces();
+    windowActivatedWithFocus(window) {
+        if (window.get_workspace().index() != this._getActiveWorkspaceIndex()) {
+            let monitor = window.get_monitor();
+            maybeLog(`next active monitor will be ${monitor}`)
+            this._monitorActivatedWithFocus = monitor;
+        }
+    }
 
-    const nextWorkspace = global.workspace_manager.get_active_workspace_index();
-    const direction = nextWorkspace > this._activeWorkspaceIndex ? DOWN : UP;
-    const diff =
-      nextWorkspace > this._activeWorkspaceIndex
-        ? nextWorkspace - this._activeWorkspaceIndex
-        : this._activeWorkspaceIndex - nextWorkspace;
-    const shift = direction * diff;
+    switchToPreviouslyActiveWorkspaceOnInactiveMonitors() {
+        if (!this._configurationService.automaticSwitchingIsEnabled()) {
+            maybeLog(`not switching to previously active workspace because automaticSwitching is disabled`);
+            return
+        }
 
-    const focusedMonitorIndex = this._monitorActivatedWithFocus != null
-        ? this._monitorActivatedWithFocus
-        : this._getFocusedMonitor();
+        this._initNWorskpaces();
 
-    this._monitorActivatedWithFocus = null
-    this._activeWorkspaceIndex = nextWorkspace;
+        const nextWorkspace = this._getActiveWorkspaceIndex();
 
-    maybeLog(` workspaceChanged
-        direction: ${direction}
-        diff: ${diff}
-        activeMonitor: ${focusedMonitorIndex}
-    `);
+        const direction = nextWorkspace > this._activeWorkspaceIndex ? DOWN : UP;
 
-    this._getWindowWrappers()
-      .filter((it) => it.isNormal())
-      .filter((it) => it.initialMonitorIndex != focusedMonitorIndex)
-      .forEach((it) => {
-        this._moveToDirection(it, shift);
-      });
+        const diff =
+            nextWorkspace > this._activeWorkspaceIndex
+                ? nextWorkspace - this._activeWorkspaceIndex
+                : this._activeWorkspaceIndex - nextWorkspace;
+        const shift = direction * diff;
 
-    maybeLog("moved some windows around");
-  }
+        const focusedMonitorIndex = this._monitorActivatedWithFocus != null
+            ? this._monitorActivatedWithFocus
+            : this._getFocusedMonitor();
 
-  switchWorkspaceOnActiveMonitor(direction) {
-    maybeLog("begin  switchActiveWorkspace");
+        this._monitorActivatedWithFocus = null
 
-    this._initNWorskpaces();
+        this._activeWorkspaceIndex = nextWorkspace;
 
-    let wrappers = this._getWindowWrappers();
+        maybeLog(` workspaceChanged
+            direction: ${direction}
+            diff: ${diff}
+            activeMonitor: ${focusedMonitorIndex}
+        `);
 
-    maybeLog(`  got ${wrappers.length} windows`);
-    wrappers.forEach((it) => {
-      maybeLog(it.toString());
-    });
+        this._getWindowWrappers()
+            .filter((it) => it.isNormal())
+            .filter((it) => it.initialMonitorIndex != focusedMonitorIndex)
+            .forEach((it) => {
+                this._moveToDirection(it, shift);
+            });
 
-    let focusedMonitorIndex = this._getFocusedMonitor();
+        maybeLog("switched to previously active workspace on inactive monitors");
+    }
 
-    let windowsToMove = wrappers
-      .filter((it) => it.isNormal())
-      .filter((it) => it.initialMonitorIndex === focusedMonitorIndex);
+    switchWorkspaceOnActiveMonitor(direction) {
+        maybeLog("begin  switchActiveWorkspace");
 
-    maybeLog(" those windows will be moved: ");
-    windowsToMove.forEach((it) => {
-      maybeLog(it.toString());
-    });
+        this._initNWorskpaces();
 
-    windowsToMove.forEach((it) => this._moveToDirection(it, direction));
-  }
+        let wrappers = this._getWindowWrappers();
 
-  _getWindowWrappers() {
-    return global.get_window_actors().map((x) => new WindowWrapper(x, x, x));
-  }
+        maybeLog(`  got ${wrappers.length} windows`);
+        wrappers.forEach((it) => {
+            maybeLog(it.toString());
+        });
 
-  _getFocusedMonitor() {
-    return global.display.get_current_monitor();
-  }
+        let focusedMonitorIndex = this._getFocusedMonitor();
+
+        let windowsToMove = wrappers
+            .filter((it) => it.isNormal())
+            .filter((it) => it.initialMonitorIndex === focusedMonitorIndex);
+
+        maybeLog(" those windows will be moved: ");
+        windowsToMove.forEach((it) => {
+            maybeLog(it.toString());
+        });
+
+        windowsToMove.forEach((it) => this._moveToDirection(it, direction));
+    }
+
+    _getWindowWrappers() {
+        return global.get_window_actors().map((x) => new WindowWrapper(x));
+    }
+
+    _getFocusedMonitor() {
+        return global.display.get_current_monitor();
+    }
+
+    _initNWorskpaces() {
+        this._nWorkspaces = global.workspace_manager.get_n_workspaces();
+        maybeLog(`  workspacesService.nWorkspaces: ${this._nWorkspaces} `);
+    }
+
+    _moveToDirection(windowWrapper, direction) {
+        let nextWorkspace = (this._nWorkspaces + windowWrapper.getWorkspaceIndex() + direction) % this._nWorkspaces;
+        maybeLog(`window: ${windowWrapper.title} will be moved to  workspace will be ${nextWorkspace}`);
+        windowWrapper.moveToWorkSpace(nextWorkspace);
+    }
+
+    _getActiveWorkspaceIndex() {
+        return global.workspace_manager.get_active_workspace_index();
+    }
 }
 
 class Controller {
-  constructor(workspaceService) {
-    this._workspaceService = workspaceService;
-    this._gsettings = ExtensionUtils.getSettings(SCHEMA);
-  }
+    constructor(workspaceService) {
+        this._workspaceService = workspaceService;
+        this._gsettings = ExtensionUtils.getSettings(SCHEMA);
+    }
 
-  up() {
-    this._workspaceService.switchWorkspaceOnActiveMonitor(UP);
-  }
+    up() {
+        this._workspaceService.switchWorkspaceOnActiveMonitor(UP);
+    }
 
-  down() {
-    this._workspaceService.switchWorkspaceOnActiveMonitor(DOWN);
-  }
+    down() {
+        this._workspaceService.switchWorkspaceOnActiveMonitor(DOWN);
+    }
 }
 
 class ConfigurationService {
-  _incompatibleExtensions = [
-    "dash-to-dock@micxgx.gmail.com",
-    "ubuntu-dock@ubuntu.com",
-  ];
 
-  constructor() {
-    this.conditionallyEnableAutomaticSwitching();
-  }
+    constructor() {
+        this._appActivateHasRightImplementation = false;
+        this._staticWorkspaces = false
+    }
 
-  conditionallyEnableAutomaticSwitching() {
+    conditionallyEnableAutomaticSwitching() {
+        this._appActivateHasRightImplementation = Shell.App.prototype.activate == overridenAppActivate
+        this._staticWorkspaces = true
+        maybeLog(this.toString())
+    }
 
-    this._anIncompatibleExtensionIsActive =
-      this._incompatibleExtensions.some((it) => {
-        let extension = imports.ui.main.extensionManager.lookup(it)
-        return (extension && extension.state == ExtensionUtils.ExtensionState.ENABLED)
-      });
+    automaticSwitchingIsEnabled() {
+        return this._appActivateHasRightImplementation && this._staticWorkspaces
+    }
 
-    this._dynamicWorspaces = Meta.prefs_get_dynamic_workspaces();
-  }
-
-  automaticSwitchingIsEnabled() {
-    return true
-    //return !this._anIncompatibleExtensionIsActive && !this._dynamicWorspaces;
-  }
-
-  toString() {
-   return `
-      anIncompatibleExtensionIsActive: ${this._anIncompatibleExtensionIsActive} 
-      dynamicWorspaces: ${this._dynamicWorspaces}`
-  }
+    toString() {
+        return `
+                automaticSwitchingIsEnabled: ${this.automaticSwitchingIsEnabled()} 
+                appActivateHasRightImplementation: ${this._appActivateHasRightImplementation} 
+                staticWorkspaces: ${this._staticWorkspaces}
+                `
+    }
 }
-
-function addKeybinding() {
-  let modeType = Shell.ActionMode.ALL;
-
-  Main.wm.addKeybinding(
-    HOTKEY_NEXT,
-    controller._gsettings,
-    Meta.KeyBindingFlags.NONE,
-    modeType,
-    controller.up.bind(controller)
-  );
-
-  Main.wm.addKeybinding(
-    HOTKEY_PREVIOUS,
-    controller._gsettings,
-    Meta.KeyBindingFlags.NONE,
-    modeType,
-    controller.down.bind(controller)
-  );
-}
-
-function removeKeybinding() {
-  Main.wm.removeKeybinding(HOTKEY_NEXT);
-  Main.wm.removeKeybinding(HOTKEY_PREVIOUS);
-}
-
-let controller;
-let workspaceService;
-let configurationService;
-let workSpaceChangedListener;
-let originalActivateWithFocus
-let originalActivate
-let originalActivateWithWorkspace
-let originalAppActivate
 
 function onWorkspaceChanged() {
   workspaceService.switchToPreviouslyActiveWorkspaceOnInactiveMonitors();
 }
 
-function onExtensionStateChanged(extension,state) {
-
-  maybeLog (`an extension state changed ${extension.uid}, ${state.state}`)
+function onExtensionStateChanged(extension, state) {
+  maybeLog(`an extension state changed ${extension.uid}, ${state.state}`)
   configurationService.conditionallyEnableAutomaticSwitching();
-  maybeLog (configurationService.toString())
 }
 
-function activateWithFocus (window, timestamp) {
-  log(`in overridden activate_with_focus`)
-
-  workspaceService.windowActivatedWithFocus(window)
-  return originalActivateWithFocus.call(this, window, timestamp);
-}
-
-function myActivate (arg1, arg2, arg3) {
-  log(`in overridden activate`)
-  workspaceService.windowActivatedWithFocus(this)
-  return originalActivate.call(this, arg1);
-}
-
-function myActivateWithWorkspace (arg1, arg2) {
-  log(`in overridden activate with workspace`)
-
-  return originalActivateWithWorkspace.call(this, arg1, arg2);
-}
-
-function appActivate (arg1, arg2, arg3) {
-  log("OVERRIDDEN APP ACTIVATE")
-  let activeWindows = this.get_windows() ;
+function overridenAppActivate() {
+  maybeLog(`overridden App::activate`)
+  let activeWindows = this.get_windows();
   workspaceService.windowActivatedWithFocus(activeWindows[0]);
   return originalAppActivate.call(this);
-
 }
 
+let controller;
+let workspaceService;
+let configurationService;
+let originalAppActivate
+let workSpaceChangedListener;
+let extensionStateChangedListener;
+
 function enable() {
-
-  originalAppActivate =  Shell.App.prototype.activate;
-  Shell.App.prototype.activate = appActivate
-
-  originalActivateWithFocus = Meta.Workspace.prototype.activate_with_focus;
-  Meta.Workspace.prototype.activate_with_focus = activateWithFocus
-
-  originalActivate = Meta.Window.prototype.activate;
-  Meta.Window.prototype.activate = myActivate
-
-  originalActivateWithWorkspace = Meta.Window.prototype.activate_with_workspace;
-  Meta.Window.prototype.activate_with_workspace = myActivateWithWorkspace
-
   configurationService = new ConfigurationService();
   workspaceService = new WorkSpacesService(configurationService);
   controller = new Controller(workspaceService);
+
+  originalAppActivate = Shell.App.prototype.activate;
+  Shell.App.prototype.activate = overridenAppActivate
+
   workSpaceChangedListener = global.workspace_manager.connect(
-    "active-workspace-changed",
-    onWorkspaceChanged
+      "active-workspace-changed",
+      onWorkspaceChanged
   );
 
   extensionStateChangedListener = Main.extensionManager.connect(
-    "extension-state-changed",
-    onExtensionStateChanged
+      "extension-state-changed",
+      onExtensionStateChanged
   );
 
-
-
-
   addKeybinding();
+  configurationService.conditionallyEnableAutomaticSwitching();
 
-  maybeLog(configurationService.toString())
 }
-
-
 
 function disable() {
   removeKeybinding();
+
   if (workSpaceChangedListener) {
     global.workspace_manager.disconnect(workSpaceChangedListener);
   }
+
   if (extensionStateChangedListener) {
     global.workspace_manager.disconnect(extensionStateChangedListener);
   }
 
-  if (originalActivateWithFocus) {
-    Meta.Workspace.prototype.activate_with_focus = originalActivateWithFocus
+  if (originalAppActivate) {
+    Shell.App.prototype.activate = originalAppActivate
   }
 
-  if (originalActivate) {
-    Shell.App.prototype.activate = originalActivate
-  }
-
-  workSpaceChangedListener = null;
-  extensionStateChangedListener = null;
   controller = null;
   workspaceService = null;
+  configurationService = null;
+  originalAppActivate = null
+  workSpaceChangedListener = null;
+  extensionStateChangedListener = null;
+}
+
+function addKeybinding() {
+    let modeType = Shell.ActionMode.ALL;
+
+    Main.wm.addKeybinding(
+        HOTKEY_NEXT,
+        controller._gsettings,
+        Meta.KeyBindingFlags.NONE,
+        modeType,
+        controller.up.bind(controller)
+    );
+
+    Main.wm.addKeybinding(
+        HOTKEY_PREVIOUS,
+        controller._gsettings,
+        Meta.KeyBindingFlags.NONE,
+        modeType,
+        controller.down.bind(controller)
+    );
+}
+
+function removeKeybinding() {
+    Main.wm.removeKeybinding(HOTKEY_NEXT);
+    Main.wm.removeKeybinding(HOTKEY_PREVIOUS);
 }
 
 function maybeLog(value) {
-  if (DEBUG_ACTIVE) {
-    log(value);
-  }
+    if (DEBUG_ACTIVE) {
+        log(value);
+    }
 }
